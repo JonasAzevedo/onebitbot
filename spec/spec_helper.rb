430b24db95598e63b52f0d3bce17405414c24a34
JonasAzevedo <jonassazevedo@gmail.com>
2017-07-19 22:21:27 -0300

    Services created

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require_relative '../app.rb'
 require 'rspec'
 require 'rack/test'
 require 'ffaker'
-#require 'pg_search'
+require 'pg_search'
 #importa todos os arquivos que estão na pasta support
 Dir["./spec/support/**/*.rb"].each { |file| require file }
 Dir["./app/services/**/*.rb"].each {|file| require file }
