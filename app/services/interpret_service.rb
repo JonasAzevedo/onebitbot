085d5cc5471661f4bf73677eb3f849138649ca96
JonasAzevedo <jonassazevedo@gmail.com>
2017-09-04 21:24:38 -0300

    Inicio desafio agregeador de links

--- app/services/interpret_service.rb
+++ app/services/interpret_service.rb
@@ -9,6 +9,12 @@ class InterpretService
       FaqModule::RemoveService.new(params).call()
     when "help"
       HelpService.call()
+    when "test_link_list", "test_link_search", "test_link_search_by_hashtag"
+      TestLinkModule::ListService.new(params, action).call()
+    when "test_link_create"
+      TestLinkModule::CreateService.new(params).call()
+    when "test_link_remove"
+      TestLinkModule::RemoveService.new(params).call()      
     else
       "Não compreendi o seu desejo"
     end
---
+++ app/services/testLink/list_service.rb
@@ -0,0 +1,36 @@
+module TestLinkModule
+  class ListService
+    def initialize(params, action)
+      # TODO: identify origin and set company
+      @company = Company.last
+      @action = action
+      @query = params["query"]
+    end
+
+    def call
+      if @action == "test_link_search"
+        links = Link.search(@query).where(company: @company)
+      elsif @action == "test_link_search_by_hashtag"
+        faqs = []
+        @company.links.each do |link|
+          link.hashtags.each do |hashtag|
+            links << link if hashtag.name == @query
+          end
+        end
+      else @action == "test_link_list"
+        links = @company.links
+      end
+
+      response = "*Link's* \n\n"
+      links.each do |link|
+        response += "*#{link.id}* - "
+        response += "*#{link.link}*\n"
+        link.hashtags.each do |h|
+          response += "_##{link.name}_ "
+        end
+        response += "\n\n"
+      end
+      (links.count > 0)? response : "Nenhum link encontrado"
+    end
+  end
+end
