2bbb2531ede31a5422f4bbcf46325415fa532d8a
JonasAzevedo <jonassazevedo@gmail.com>
2017-09-04 21:53:11 -0300

    Corrigido presence_of link

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,7 +2,7 @@ require "pg_search"
 include PgSearch
 
 class Link < ActiveRecord::Base
-  validates_presence_of :question, :answer
+  validates_presence_of :link
 
   has_many :link_hashtags
   has_many :hashtags, through: :link_hashtags
