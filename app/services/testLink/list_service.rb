1d172df110af27fd946eab9c8021a8c2cc44e8b8
JonasAzevedo <jonassazevedo@gmail.com>
2017-10-10 08:35:45 -0300

    Teste

--- app/services/testLink/list_service.rb
+++ app/services/testLink/list_service.rb
@@ -8,18 +8,7 @@ module TestLinkModule
     end
 
     def call
-      links = @company.links
-      
-      response = "*Link's* \n\n"
-      links.each do |link|
-        response += "*#{link.id}* - "
-        response += "*#{link.link}*\n"
-        link.hashtags.each do |h|
-          response += "_##{link.name}_ "
-        end
-        response += "\n\n"
-      end
-      (links.count > 0)? response : "Nenhum link encontrado"
+      response = "saca"
     end
   end
 end
