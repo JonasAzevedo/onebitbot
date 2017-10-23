f155216f9311d8839d194922778fd073e57b744e
JonasAzevedo <jonassazevedo@gmail.com>
2017-10-23 20:43:40 -0200

    Testando

--- app/services/link/list_service.rb
+++ app/services/link/list_service.rb
@@ -9,8 +9,8 @@ module LinkModule
 
     def call
       if @action == "link_search"
-        #links = Link.search(@query) # .where(company: @company)
-        faqs = Faq.search(@query).where(company: @company)
+        links = Link.search('google') # .where(company: @company)
+        #faqs = Faq.search(@query).where(company: @company)
       elsif @action == "link_search_by_hashtag"
         links = []
         @company.links.each do |link|
