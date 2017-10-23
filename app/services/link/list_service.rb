1b46c8887274c49bddc2c64767101b3b9a433666
JonasAzevedo <jonassazevedo@gmail.com>
2017-10-23 20:36:53 -0200

    Testando

--- app/services/link/list_service.rb
+++ app/services/link/list_service.rb
@@ -9,7 +9,8 @@ module LinkModule
 
     def call
       if @action == "link_search"
-        links = Link.search(@query) # .where(company: @company)
+        #links = Link.search(@query) # .where(company: @company)
+        faqs = Faq.search(@query).where(company: @company)
       elsif @action == "link_search_by_hashtag"
         links = []
         @company.links.each do |link|
@@ -21,16 +22,17 @@ module LinkModule
         links = @company.links
       end
 
-      response = "*Link's* \n\n"
-      links.each do |link|
-        response += "*#{link.id}* - "
-        response += "*#{link.link}*\n"
-        link.hashtags.each do |h|
+      response = "*Perguntas e Respostas* \n\n"
+      faqs.each do |f|
+        response += "*#{f.id}* - "
+        response += "*#{f.question}*\n"
+        response += ">#{f.answer}\n"
+        f.hashtags.each do |h|
           response += "_##{h.name}_ "
         end
         response += "\n\n"
       end
-      (links.count > 0)? response : "Nada encontrado"
+      (faqs.count > 0)? response : "Nada encontrado"
     end
   end
 end
